b6dc7fe390bb310aa5fe29eb6edeca9e7aa3bb43
Subha Das <rohan6056@hotmail.com>
2022-09-15 23:23:58 -0400

    Fixed misplaced jsdoc comments

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+const allowedEmails = process.env.ALLOWED_EMAILS.split("+");
+
 /** Configuration for NextAuth
  * SignIn callback only allows emails specfied in environment
  * variables to log in
  */
-
-const allowedEmails = process.env.ALLOWED_EMAILS.split("+");
-
 export const authOptions = {
   providers: [
     GoogleProvider({
