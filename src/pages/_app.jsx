4b1a2bad485e30c1afb66a173df05630505d3d92
mhartlage3 <madeline.hartlage@gatech.edu>
2022-09-17 16:49:47 -0400

    added chakra ui, edited app css, added login ui

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { SWRConfig } from "swr";
+import {ChakraProvider} from "@chakra-ui/react"
 
 import Header from "src/components/Header";
 import fetchJson from "src/utils/lib/fetchJson";
@@ -10,7 +11,11 @@ import "focus-visible/dist/focus-visible.min.js";
 import "normalize.css";
 import "public/static/styles/App.css";
 
-const MyApp = ({ Component, pageProps }) => (
+const MyApp = ({ Component, pageProps }) => {
+  const PAGES_WITH_NO_HEADER = new Set(["LoginPage"]);
+  const renderHeader = !PAGES_WITH_NO_HEADER.has(Component.name);
+
+  return (
   <SWRConfig
     value={{
       fetcher: fetchJson,
@@ -21,13 +26,16 @@ const MyApp = ({ Component, pageProps }) => (
       <title>Next.js-Starter</title>
     </Head>
     <div className="App">
-      <Header />
+      {renderHeader && <Header />}
       <div className="Content">
-        <Component {...pageProps} />
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </div>
     </div>
   </SWRConfig>
-);
+  );
+  };
 
 MyApp.propTypes = {
   Component: PropTypes.any.isRequired,
