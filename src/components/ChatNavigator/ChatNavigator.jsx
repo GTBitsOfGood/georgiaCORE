9ed4349a32b19108d3eac4daa0245fffc2c8550a
Manu Gupta <mgupta@lawn-143-215-52-105.lawn.gatech.edu>
2022-10-31 02:19:11 -0400

    Made changes to preview navigator

--- src/components/ChatNavigator/ChatNavigator.jsx
+++ src/components/ChatNavigator/ChatNavigator.jsx
@@ -1,18 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import styles from "./ChatNavigator.module.css";
-import { getActiveQuestionTree } from "../../actions/Tree";
+import { getActiveQuestionTree, getQuestionTreeById } from "../../actions/Tree";
 import Image from "next/image";
 import PropTypes from "prop-types";
 import QuestionTemplate from "../QuestionTemplate/QuestionTemplate";
+import { useRouter } from "next/router";
+import ErrorPage from "../ErrorPage";
 
 const ChatNavigator = (props) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
   const [allQuestions, setAllQuestions] = useState({});
+  const [invalidID, setInvalidId] = useState(false);
+
+  const router = useRouter()
+  const { query } = router;
+
   useEffect(() => {
     const setup = async () => {
       try {
-        const questionsTree = await getActiveQuestionTree();
+        const questionsTree = await getQuestionTreeById(query.id);
         const questions = questionsTree.questions;
         const questionMap = {};
         for (const question of questions) {
@@ -20,14 +27,24 @@ const ChatNavigator = (props) => {
           questionMap[id] = question;
         }
         setAllQuestions(questionMap);
+        setInvalidId(false);
       } catch {
+        setInvalidId(true);
         console.log("Unable to get questions at this time.");
       }
     };
     setup();
-  }, []);
+  }, [router.isReady]);
 
   const numQs = Object.keys(allQuestions).length;
+
+  if (invalidID) {
+    return (
+      <>
+        <ErrorPage message="This tree does not exist." />
+      </>
+    );
+  }
 
   if (numQs > 0) {
     const currentQuestion = allQuestions[currentQuestionIndex];
