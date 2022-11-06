4e90c4d4ede5da1412afb861c45cd23ab0ef3e8e
mhartlage3 <madeline.hartlage@gatech.edu>
2022-11-06 04:27:48 -0500

    progress bar and home/back button designed and functional

--- src/components/ChatNavigator/ChatNavigator.jsx
+++ src/components/ChatNavigator/ChatNavigator.jsx
@@ -4,10 +4,13 @@ import styles from "./ChatNavigator.module.css";
 import { getActiveQuestionTree, getQuestionTreeById } from "../../actions/Tree";
 import Image from "next/image";
 import PropTypes from "prop-types";
+import { IoChevronBackOutline } from "react-icons/io5";
+import { MdHome } from "react-icons/md";
 import QuestionTemplate from "../QuestionTemplate/QuestionTemplate";
 import { useRouter } from "next/router";
 import ErrorPage from "../ErrorPage";
 import { useSession } from "next-auth/react";
+import { Text, Flex, Stack } from "@chakra-ui/react";
 
 const ChatNavigator = (props) => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(1);
@@ -40,6 +43,11 @@ const ChatNavigator = (props) => {
   }, [router.isReady]);
 
   const numQs = Object.keys(allQuestions).length;
+  
+  const progressBars = []
+  for (let i = 0; i < numQs; i++){
+    progressBars.push(i)
+  }
 
   if (status === "loading") {
     return <></>;
@@ -64,25 +72,94 @@ const ChatNavigator = (props) => {
   if (numQs > 0) {
     const currentQuestion = allQuestions[currentQuestionIndex];
     return (
-      <div style={styles} id={styles.main}>
-        <div
-          id={styles.progressBar}
-          style={{ width: ((currentQuestionIndex - 1) / numQs) * 100 + "%" }}
-        ></div>
-        <QuestionTemplate
-          question={currentQuestion.question}
-          options={currentQuestion.options.map((option) => {
-            return {
-              answer: option.option,
-              triggerNext: () => {
-                if (option.nextId) {
-                  setCurrentQuestionIndex(option.nextId);
-                }
-              },
-            };
-          })}
-        ></QuestionTemplate>
-      </div>
+      <Flex
+        width="100%"
+        height="100%"
+        backgroundColor="#F9F8FA"
+      >
+        <Stack
+          direction="column"
+          width="100%"
+        >
+          <Stack
+            direction="row" 
+            width="100%"
+            backgroundColor="#F9F8FA" 
+            justifyContent="center"
+            alignItems="center"
+            paddingTop={5}
+            paddingBottom={5}
+          >
+            <Stack  direction="row" width="33%" paddingLeft={20}>
+              <Flex 
+                backgroundColor="#343A40"
+                alignItems="center"
+                justifyContent="center"
+                borderRadius={2}
+                height={26}
+                width={26}
+                onClick={() => setCurrentQuestionIndex(1)}
+                cursor="pointer"
+              >
+                <MdHome size={15} color="white"/>
+              </Flex>
+              <Flex 
+                backgroundColor="#343A40"
+                alignItems="center"
+                justifyContent="center"
+                borderRadius={2}
+                height={26}
+                width={26}
+                onClick={() => currentQuestionIndex > 1 ? setCurrentQuestionIndex(currentQuestionIndex - 1): setCurrentQuestionIndex(1)}
+                cursor="pointer"
+              >
+                <IoChevronBackOutline size={15} color="white"/>
+              </Flex>
+            </Stack>
+            <Flex width="33%" justifyContent="center">
+              <Text
+                textAlign= "center"
+                marginBottom={0}
+                color= "#343A40"
+                fontSize= "28px"
+                fontWeight= {600}
+                fontFamily="sans-serif"
+              >
+                {currentQuestion.question}
+              </Text>
+            </Flex>
+            <Flex width="33%" justifyContent="flex-end" paddingRight={20}>
+              <Stack direction="row" width="25%">
+                {progressBars.map((index) => {
+                  return (
+                    <Flex 
+                      key={index} 
+                      backgroundColor={currentQuestionIndex - 1 >= index ? "#58794E" : "#343A40"} 
+                      opacity={currentQuestionIndex - 1 >= index ? 1 : .25} 
+                      width="25px" 
+                      height="6px"
+                    >
+                    </Flex>
+                  );
+                })}
+              </Stack>
+            </Flex>
+          </Stack>
+          <QuestionTemplate
+            question={currentQuestion.question}
+            options={currentQuestion.options.map((option) => {
+              return {
+                answer: option.option,
+                triggerNext: () => {
+                  if (option.nextId) {
+                    setCurrentQuestionIndex(option.nextId);
+                  }
+                },
+              };
+            })}
+          ></QuestionTemplate>
+        </Stack>
+      </Flex>
     );
   }
 
--- src/components/QuestionTemplate/QuestionTemplate.jsx
+++ src/components/QuestionTemplate/QuestionTemplate.jsx
@@ -1,144 +1,86 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Flex, Text, Stack } from "@chakra-ui/react";
-import styles from "./QuestionTemplate.module.css";
-import Image from "next/image";
 import PropTypes from "prop-types";
 import { PhoneIcon } from "@chakra-ui/icons";
 
 const QuestionTemplate = (props) => {
   return (
-    <Flex
-      width="100%"
-      height="100%"
-      backgroundColor="#F9F8FA"
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center"
+      paddingBottom={50}
     >
-      <Stack
-        direction="column"
-        width="100%"
-      >
-        <Flex 
-          width="100%" 
-          backgroundColor="#F9F8FA" 
-          justifyContent="center"
-        >
-          <Text
-            paddingTop= "30px"
-            paddingBottom= "10px"
-            textAlign= "center"
-            marginBottom= "0px"
-            color= "#343A40"
-            fontSize= "28px"
-            fontWeight= {600}
-            fontFamily="sans-serif"
+      {props.options.map((option, index) => {
+        return (
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            key={index}
+            onClick={option.triggerNext ? option.triggerNext : () => {}}
+            padding="10px"
+            width= "250px"
+            height="250px"
+            marginRight="20px"
+            marginLeft="20px"
+            
+            cursor="pointer"
           >
-            {props.question}
-          </Text>
-        </Flex>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          textAlign="center"
-          paddingBottom="50px"
-        >
-          {props.options.map((option, index) => {
-            return (
-              <Stack
-                direction="column"
-                justifyContent="center"
+            {props.options.image ? (
+              <Flex
+                backgroundColor= "#5B794E"
+                width="120px"
+                height="120px"
+                borderRadius="100%"
                 alignItems="center"
-                key={index}
-                onClick={option.triggerNext ? option.triggerNext : () => {}}
-                padding="10px"
-                width= "250px"
-                height="250px"
-                marginRight="20px"
-                marginLeft="20px"
-                
-                cursor="pointer"
+                justifyContent="center"
+                color="white"
+                marginBottom="20px"
               >
-                {props.options.image ? (
-                  <Flex
-                    backgroundColor= "#5B794E"
-                    width="120px"
-                    height="120px"
-                    borderRadius="100%"
-                    alignItems="center"
-                    justifyContent="center"
-                    color="white"
-                    marginBottom="20px"
-                  >
-                    <PhoneIcon></PhoneIcon>
-                  </Flex>
-                ) : (
-                  <Flex
-                    backgroundColor= "#5B794E"
-                    width="120px"
-                    height="120px"
-                    borderRadius="100%"
-                    alignItems="center"
-                    justifyContent="center"
-                    color="white"
-                  >
-                    <PhoneIcon height={50} width={50}></PhoneIcon>
-                  </Flex>
-                )}
-                <Flex width="100%" backgroundColor="#F9F8FA" justifyContent="center">
-                  <Text
-                    color="#343A40"
-                    fontSize="24px"
-                    fontWeight={700}
-                    fontFamily="sans-serif"
-                  >
-                    {option.answer}
-                  </Text>
-                </Flex>
-                <Flex width="100%" backgroundColor="#F9F8FA" justifyContent="center">
-                  <Text
-                    color= "#343A40"
-                    opacity={0.7}
-                    fontSize="16px"
-                    lineHeight={1.3}
-                    fontWeight={600}
-                    fontFamily="sans-serif"
-                  >
-                    professional medical resources that may help
-                  </Text>
-                </Flex>
-              </Stack>
-            );
-          })}
-        </Stack>
-      </Stack>
-    </Flex>
-    /*<div style={styles} id={styles.container} >
-      <h2 id={styles.question}>{props.question}</h2>
-      <div id={styles.optionContainer}>
-        {props.options.map((option, index) => {
-          return (
-            <div
-              key={index}
-              id={styles.optionStack}
-              onClick={option.triggerNext ? option.triggerNext : () => {}}
-            >
-              {props.options.image ? (
-                <div id={styles.greenCircle}>
-                  <PhoneIcon></PhoneIcon>
-                </div>
-              ) : (
-                <div id={styles.greenCircle}>
-                   <PhoneIcon height={50} width={50}></PhoneIcon>
-                </div>
-              )}
-              <h4 id={styles.optionAnswer}>{option.answer}</h4>
-              <p id={styles.optionInfo}>professional medical resources that may help</p> 
-              <div />
-            </div>
-          );
-        })}
-      </div>
-    </div>*/
+                <PhoneIcon></PhoneIcon>
+              </Flex>
+            ) : (
+              <Flex
+                backgroundColor= "#5B794E"
+                width="120px"
+                height="120px"
+                borderRadius="100%"
+                alignItems="center"
+                justifyContent="center"
+                color="white"
+              >
+                <PhoneIcon height={50} width={50}></PhoneIcon>
+              </Flex>
+            )}
+            <Flex width="100%" backgroundColor="#F9F8FA" justifyContent="center">
+              <Text
+                color="#343A40"
+                fontSize="24px"
+                fontWeight={700}
+                fontFamily="sans-serif"
+              >
+                {option.answer}
+              </Text>
+            </Flex>
+            <Flex width="100%" backgroundColor="#F9F8FA" justifyContent="center">
+              <Text
+                color= "#343A40"
+                opacity={0.7}
+                fontSize="16px"
+                lineHeight={1.3}
+                fontWeight={600}
+                fontFamily="sans-serif"
+              >
+                professional medical resources that may help
+              </Text>
+            </Flex>
+          </Stack>
+        );
+      })}
+    </Stack>
   );
 };
 
