0d797d380bbea1af432fe8d50be81751822bd20a
Manu Gupta <mgupta@Manus-MacBook-Pro-10.local>
2022-11-25 18:03:01 -0500

    Add more icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,27 +1,95 @@
-import { FaInstagram, FaFacebook, FaQuestion } from "react-icons/fa";
-import { GrResources, GiMedicalThermometer } from "react-icons/gr";
-import { BiPlusMedical } from "react-icons/bi";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaQuestion,
+  FaAccessibleIcon,
+  FaUserNurse,
+  FaStethoscope,
+  FaBrain,
+} from "react-icons/fa";
+import {
+  GrResources,
+  GiMedicalThermometer,
+  GrNavigate,
+  GiCaduceus,
+} from "react-icons/gr";
+import { BiPlusMedical, BiNavigation } from "react-icons/bi";
 import { CiMedicalMask } from "react-icons/ci";
-import { BsFileMedical } from "react-icons/bs";
+import { IoBandage } from "react-icons/io";
+import { BsFileMedical, BsSearch } from "react-icons/bs";
 import { MdCancel, MdPersonPin } from "react-icons/md";
-import { AiOutlineCalendar } from "react-icons/ai";
-import { GiPersonInBed } from "react-icons/gi";
+import {
+  AiOutlineCalendar,
+  AiFillEnvironment,
+  AiFillAlert,
+  AiFillCloud,
+  AiFillEdit,
+  AiFillFolder,
+  AiFillFire,
+  AiFillEye,
+  AiFillGoogleSquare,
+  AiFillInfoCircle,
+  AiFillFrown,
+  AiFillHome,
+  AiFillHeart,
+  AiFillMessage,
+  AiFillSmile,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+import {
+  GiPersonInBed,
+  GiMedicines,
+  GiInspiration,
+  GiBandageRoll,
+  GiLungs,
+  GiKidneys,
+} from "react-icons/gi";
+import { TbVaccine } from "react-icons/tb";
 import React from "react";
 
 const icons = {
+  Alert: <AiFillAlert />,
+  Bandage: <IoBandage />,
+  BandageRoll: <GiBandageRoll />,
+  Brain: <FaBrain />,
+  Caduceus: <GiCaduceus />,
   Calendar: <AiOutlineCalendar />,
   Cancel: <MdCancel />,
+  Cloud: <AiFillCloud />,
   Doctor: <MdPersonPin />,
+  Edit: <AiFillEdit />,
+  Eye: <AiFillEye />,
   Facebook: <FaFacebook />,
+  Fire: <AiFillFire />,
+  Frown: <AiFillFrown />,
+  Folder: <AiFillFolder />,
+  Google: <AiFillGoogleSquare />,
+  Heart: <AiFillHeart />,
+  Home: <AiFillHome />,
+  Information: <AiFillInfoCircle />,
   Instagram: <FaInstagram />,
+  Inspiration: <GiInspiration />,
+  Kidney: <GiKidneys />,
+  Location: <AiFillEnvironment />,
+  Lungs: <GiLungs />,
   Mask: <CiMedicalMask />,
+  Medicine: <GiMedicines />,
   MedicalForm: <BsFileMedical />,
   MedicalPlus: <BiPlusMedical />,
+  Message: <AiFillMessage />,
+  Navigate: <GrNavigate />,
+  NavigateArrow: <BiNavigation />,
+  Nurse: <FaUserNurse />,
   PersonInBed: <GiPersonInBed />,
   QuestionMark: <FaQuestion />,
   Resources: <GrResources />,
+  Search: <BsSearch />,
+  Smile: <AiFillSmile />,
+  Sthetoscope: <FaStethoscope />,
   Thermometer: <GiMedicalThermometer />,
-  
+  Twitter: <AiFillTwitterCircle />,
+  Wheelchair: <FaAccessibleIcon />,
+  Vaccine: <TbVaccine />,
 };
 
 export default icons;
