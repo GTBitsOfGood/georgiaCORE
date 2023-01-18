0ce161794975ce6da6c7817668d09c175bab3019
Manu Gupta <mgupta8143@gmail.com>
2023-01-17 20:33:24 -0500

    Added more icons

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -6,13 +6,16 @@ import {
   FaUserNurse,
   FaStethoscope,
   FaBrain,
+  FaMoneyBill,
+  FaDonate,
+  FaHospitalAlt
 } from "react-icons/fa";
-import { GrResources, GrNavigate } from "react-icons/gr";
+import { GrResources, GrNavigate, GrUserWorker, GrGroup } from "react-icons/gr";
 import { BiPlusMedical, BiNavigation } from "react-icons/bi";
 import { CiMedicalMask } from "react-icons/ci";
 import { IoBandage } from "react-icons/io5";
-import { BsFileMedical, BsSearch } from "react-icons/bs";
-import { MdCancel, MdPersonPin } from "react-icons/md";
+import { BsFileMedical, BsSearch, BsIntersect, BsFillPersonFill, BsNewspaper } from "react-icons/bs";
+import { MdCancel, MdPersonPin, MdEmail, MdOutlineFamilyRestroom, MdRecordVoiceOver } from "react-icons/md";
 import {
   AiOutlineCalendar,
   AiFillEnvironment,
@@ -36,33 +39,45 @@ import {
   GiMedicines,
   GiInspiration,
   GiBandageRoll,
+  GiBiceps,
   GiLungs,
   GiKidneys,
   GiCaduceus,
   GiMedicalThermometer,
+  GiBookCover,
+  GiPalm
 } from "react-icons/gi";
 import { TbVaccine } from "react-icons/tb";
 import React from "react";
+
 
 const icons = {
   Alert: <AiFillAlert />,
   Bandage: <IoBandage />,
   BandageRoll: <GiBandageRoll />,
+  Biceps: <GiBiceps/>,
+  Book: <GiBookCover/>,
   Brain: <FaBrain />,
   Caduceus: <GiCaduceus />,
   Calendar: <AiOutlineCalendar />,
   Cancel: <MdCancel />,
   Cloud: <AiFillCloud />,
+  Differences: <BsIntersect/>,
   Doctor: <MdPersonPin />,
+  Donate: <FaDonate/>,
   Edit: <AiFillEdit />,
+  Email: <MdEmail/>,
   Eye: <AiFillEye />,
   Facebook: <FaFacebook />,
+  Family: <MdOutlineFamilyRestroom/>,
   Fire: <AiFillFire />,
   Frown: <AiFillFrown />,
   Folder: <AiFillFolder />,
   Google: <AiFillGoogleSquare />,
+  Group: <GrGroup/>,
   Heart: <AiFillHeart />,
   Home: <AiFillHome />,
+  Hospital: <FaHospitalAlt/>,
   Information: <AiFillInfoCircle />,
   Instagram: <FaInstagram />,
   Inspiration: <GiInspiration />,
@@ -74,18 +89,24 @@ const icons = {
   MedicalForm: <BsFileMedical />,
   MedicalPlus: <BiPlusMedical />,
   Message: <AiFillMessage />,
+  Money: <FaMoneyBill/>,
   Navigate: <GrNavigate />,
   NavigateArrow: <BiNavigation />,
+  Newspaper: <BsNewspaper/>,
   Nurse: <FaUserNurse />,
+  Person: <BsFillPersonFill/>, 
   PersonInBed: <GiPersonInBed />,
   QuestionMark: <FaQuestion />,
+  RaisedHand: <GiPalm/>,
   Resources: <GrResources />,
   Search: <BsSearch />,
   Smile: <AiFillSmile />,
+  Speaking: <MdRecordVoiceOver/>,
   Sthetoscope: <FaStethoscope />,
   Thermometer: <GiMedicalThermometer />,
   Twitter: <AiFillTwitterCircle />,
   Wheelchair: <FaAccessibleIcon />,
+  Worker: <GrUserWorker/>,
   Vaccine: <TbVaccine />,
 };
 
